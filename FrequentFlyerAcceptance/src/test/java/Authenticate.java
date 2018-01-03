a6faa163254817d7ea849fb1ab3433638f67fda9
Christophe THEPAUT <christophe@thepaut.net>
2018-01-03 10:59:23 +0100

    add logging on feature and make it fail for good reason

--- FrequentFlyerAcceptance/src/test/java/Authenticate.java
+++ FrequentFlyerAcceptance/src/test/java/Authenticate.java
@@ -1,4 +1,33 @@
-package features;
+import cucumber.api.PendingException;
+import cucumber.api.java8.En;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 
-public class Authenticate {
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class Authenticate extends IntegrationSteps implements En {
+    public Authenticate() {
+        And("^\"([^\"]*)\" has registered online with a password of \"([^\"]*)\"$", (String member, String password) -> {
+            responseEntity = restTemplate.getForEntity(createURL("/members?name="+member+"&password="+password), String.class);
+        });
+        When("^\"([^\"]*)\" logs on with password \"([^\"]*)\"$", (String member, String password) -> {
+            responseEntity = restTemplate.getForEntity(createURL("/members?name="+member+"&password="+password), String.class);
+        });
+        Then("^he should be given access to the site$", () -> {
+            assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
+        });
+        Then("^he should be informed that his password was incorrect$", () -> {
+            assertThat(responseEntity.getBody(), is("Incorrect Password"));
+        });
+        Given("^the account has expired$", () -> {
+            assertThat(responseEntity.getBody(), is("Account has expired"));
+        });
+        Then("^he should be informed that his account has expired$", () -> {
+            assertThat(responseEntity.getBody(), is("Account has expired"));
+        });
+        And("^he should be invited to renew his account$", () -> {
+            assertThat(responseEntity.getBody(), is("Renew your password"));
+        });
+    }
 }
--- FrequentFlyerAcceptance/src/test/java/RequestMember.java
+++ FrequentFlyerAcceptance/src/test/java/RequestMember.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import cucumber.api.java8.En;
 
-public class RequestMember {
+public class RequestMember extends IntegrationSteps implements En {
+
+    public RequestMember() {
+        Given("^\"([^\"]*)\" is a Frequent Flyer member$", (String member) -> {
+            responseEntity = restTemplate.getForEntity(createURL("/members?name=" + member), String.class);
+        });
+    }
 }
